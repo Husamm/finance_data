f52b6aabf23ac69afa2ee6ebfda2e808b065a5e9
Husam Massalha <husamm456@gmail.com>
2023-06-27 16:53:39 +0300

    implement get_combined_aggs

--- get_finance_data.py
+++ get_finance_data.py
@@ -13,6 +13,30 @@ class GetFinanceData:
     def __init__(self):
         self.client = RESTClient(POLYGON_API_KEY)
 
+    def get_combined_aggs(self, tickers_list, from_date, to_date):
+        if (datetime.now() - from_date).days > YEARS_BACK * 365:
+            return None
+        tickers_maps = []
+        for i in range(len(tickers_list)):
+            ticker_data = self.client.get_aggs(ticker=tickers_list[i], from_=from_date.strftime("%Y-%m-%d"),
+                                               to=to_date.strftime("%Y-%m-%d"), limit=5000, multiplier=1,
+                                               timespan='day')
+            tickers_maps.insert(i, {ticker_data[j].timestamp: ticker_data[j] for j in range(len(ticker_data))})
+        # sum all the data to first ticker dictionary
+        for i in range(1, len(tickers_list)):
+            for timestamp_i in tickers_maps[0].keys():
+                ticker_i_agg = tickers_maps[i].get(timestamp_i)
+                if ticker_i_agg is None:
+                    tickers_maps[0].pop(timestamp_i)
+                else:
+                    # sum the open,close,high,low,volume
+                    for field_name in ['open', 'close', 'high', 'low', 'volume']:
+                        curr_val = getattr(tickers_maps[0].get(timestamp_i), field_name)
+                        other_val = getattr(tickers_maps[i].get(timestamp_i), field_name)
+                        setattr(tickers_maps[0].get(timestamp_i), field_name, curr_val + other_val)
+
+        return tickers_maps[0]
+
     def get_EPS_annoucments(self, ticker):
         url = f"https://www.alphavantage.co/query?function=EARNINGS&symbol={ticker}&apikey={ALPHAVANTAGE_API_KEY}"
         response = urlopen(url)
@@ -20,7 +44,7 @@ class GetFinanceData:
         return QuarterlyEarnings.parse_json_list(data_json['quarterlyEarnings'])
 
     def get_stock_percentage_change(self, ticker, date):
-        if (datetime.now()-date).days > YEARS_BACK*365:
+        if (datetime.now() - date).days > YEARS_BACK * 365:
             return None
         try:
             aggs = self.client.get_aggs(
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from finance_analysis import EPS_announcements_to_price_change
+from datetime import date, datetime, timedelta
 
-res = EPS_announcements_to_price_change('TSLA')
+from get_finance_data import GetFinanceData
+
+print("-HHH- ")
+res = GetFinanceData().get_combined_aggs(tickers_list=['META'],from_date=datetime.today()-timedelta(days=6),to_date=datetime.today()-timedelta(days=5))
+print(res)
+
+res = GetFinanceData().get_combined_aggs(tickers_list=['TSLA'],from_date=datetime.today()-timedelta(days=6),to_date=datetime.today()-timedelta(days=5))
+print(res)
+
+res = GetFinanceData().get_combined_aggs(tickers_list=['META','TSLA'],from_date=datetime.today()-timedelta(days=6),to_date=datetime.today()-timedelta(days=5))
+print(res)
+
+#res = GetFinanceData().get_combined_aggs(tickers_list=['AAPL'],from_date=datetime.today()-timedelta(days=6),to_date=datetime.today()-timedelta(days=5))
+print(res)
+
+
+res = GetFinanceData().get_combined_aggs(tickers_list=['META','TSLA','AAPL'],from_date=datetime.today()-timedelta(days=6),to_date=datetime.today()-timedelta(days=5))
 print(res)
 
 
